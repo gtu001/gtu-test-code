bdd2f52cd1dc13a2822a1f7a74b8a6123dae45cd
gtu001 <gtu001@gmail.com>
2018-03-30 17:09:03 +0800

    vv

---
+++ GTU/src/_temp/Test40.java
@@ -0,0 +1,11 @@
+package _temp;
+
+import java.math.BigDecimal;
+
+public class Test40 {
+
+    public static void main(String[] args) {
+        System.out.println(new BigDecimal(-1).compareTo(BigDecimal.ZERO));
+    }
+
+}
