d9f8dcea6208594f54d5a4c66ac58e74e649dd39
gtu001 <gtu001@gmail.com>
2018-06-06 01:11:56 +0800

    vv

--- PythonGtu/gtu/data_science/pandas/ex3/pandas_janna_test_001.py
+++ PythonGtu/gtu/data_science/pandas/ex3/pandas_janna_test_001.py
@@ -4,11 +4,41 @@ import numpy as np
 import pandas as pd
 from gtu.io import fileUtil
 from gtu.reflect import checkSelf
+import os
 
+
+def loadExcel(filePath):
+    sheet_name = ["年中人口"]
+    headerRowIndices = [1] 
+    indexColIndices = [0, 1]
+    names = None  # 若無column title 在此自訂
+    dataFrame = pd.read_excel(filePath, sheet_name=sheet_name, header=headerRowIndices, \
+                  skiprows=None, \
+                  skip_footer=0, index_col=indexColIndices, names=names, \
+                  usecols=None, parse_dates=False, date_parser=None, \
+                  na_values=None, thousands=None, convert_float=True, \
+                  converters=None, dtype=None, true_values=None, \
+                  false_values=None, engine=None, squeeze=False)
+    return dataFrame
+
+
+def writeExcel(dataFrame, targetName):
+    targetPath = fileUtil.getDesktopDir() + os.sep + targetName
+    excelWriter = pd.ExcelWriter(targetPath)
+    dataFrame.to_excel(excelWriter, sheet_name='Sheet1', \
+             na_rep='', float_format=None, columns=None, \
+             header=True, index=True, index_label=None, \
+             startrow=0, startcol=0, engine=None, merge_cells=True, \
+             encoding=None, inf_rep='inf', verbose=True, freeze_panes=None)
 
 
 def main(filePath):
-    checkSelf.checkMembers(pd)
+    sheetSet = loadExcel(filePath)
+    df = sheetSet["年中人口"]
+    
+    for i in df.index:
+        print(df['10~14'][i])
+
     pass
 
 
