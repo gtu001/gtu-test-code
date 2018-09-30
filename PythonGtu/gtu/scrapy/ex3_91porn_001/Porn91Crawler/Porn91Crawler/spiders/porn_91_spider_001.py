5f0d170d9425910b54a9dab686afab89c46a628e
gtu001@gmail.com <gtu001@gmail.com>
2018-09-30 11:46:48 +0800

    fff

--- PythonGtu/gtu/scrapy/ex3_91porn_001/Porn91Crawler/Porn91Crawler/spiders/porn_91_spider_001.py
+++ PythonGtu/gtu/scrapy/ex3_91porn_001/Porn91Crawler/Porn91Crawler/spiders/porn_91_spider_001.py
@@ -10,9 +10,11 @@ from scrapy.settings import Settings
 from scrapy.spiders import CrawlSpider, Rule
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
 from twisted.internet import reactor
 
 from gtu._tkinter.util import tkinterUtil
@@ -38,7 +40,13 @@ def getWebDriver():
     elif type == 'chrome' :
         return webdriver.Chrome() 
     else :
-        return webdriver.Firefox()
+        binary = FirefoxBinary(r'/usr/lib/firefox/firefox.sh')
+        fireFoxOptions = webdriver.FirefoxOptions()
+        fireFoxOptions.set_headless()
+        firefox_capabilities = DesiredCapabilities.FIREFOX
+        firefox_capabilities['marionette'] = True
+        driver = webdriver.Firefox(capabilities=firefox_capabilities,firefox_binary=binary, firefox_options = fireFoxOptions)
+        return driver
 
 
 # https://medium.com/python-pandemonium/develop-your-first-web-crawler-in-python-scrapy-6b2ee4baf954
@@ -66,25 +74,28 @@ class Porn91CrawlerSpider(scrapy.Spider):  #   scrapy.Spider    /    CrawlSpider
         print("### MAIN page : ", response.url)
         
         self.driver.get(response.url)
-        print("-------------------A1")
-        
-        wait = WebDriverWait(self.driver, 5)
-        
-        print("-------------------A")
-        
-        ulObj = wait.until(ec.presence_of_element_located((By.XPATH, "//ul[contains(@class, 'jcarousel-list')]")) , "failed !!!!")
-        liLst = ulObj.find_elements_by_xpath(".//li[:]")#
-        
-        for i in liLst :
-            print("-----", i)
-            
-        print("liLst ", liLst)
-        print("liLst arry ", listUtil.isArray(liLst))
-        print("testObj ==> ", type(liLst))
-        
 #         selenium.webdriver.firefox.webelement.FirefoxWebElement
+
+        print("-------------------A")
+        self.getClickBtn()
         
         print("-------------------B")
+        
+        self.driver.close()
+
+
+    def getMovList(self):
+        wait = WebDriverWait(self.driver, 5)
+        ulObj = wait.until(ec.presence_of_element_located((By.XPATH, "//ul[contains(@class, 'jcarousel-list')]")) , "failed !!!!")
+        liLst = wait.until(ec.presence_of_all_elements_located((By.XPATH, ".//li[position()>=0 and position()<=1000]")) , "failed2 !!!!")
+        for (i,v) in enumerate(liLst) :
+            print("-----", i, v)
+            
+    def getClickBtn(self) :
+        wait = WebDriverWait(self.driver, 5)
+        btns = wait.until(ec.presence_of_all_elements_located((By.XPATH, "//div[contains(@class, 'jcarousel-next') || contains(@class, 'jcarousel-prev')]")) , "failed2 !!!!")
+        for (i,v) in enumerate(btns) :
+            print("-----", i, v)
 
 
     def doLoginForm(self):
