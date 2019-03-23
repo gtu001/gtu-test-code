58f35208750428bdbec4f371d1994d6c00cbb4b0
gtu001 <gtu001@gmail.com>
2019-03-23 13:02:04 +0800

    ok

---
+++ PythonGtu/gtu/data_science/test001/bottle1.py
@@ -0,0 +1,15 @@
+import ustruct as struct
+from bottle import route, run, template
+port = 9099
+
+@route('/personal/<name>')
+def homepage(name):
+    return template('Hi <b>{{name}}</b>!', name=name)
+
+print("Try going to http://localhost:{}/personal/Tom".format(port))
+print("Try going to http://localhost:{}/personal/Carl".format(port))
+run(host='localhost', port=port)
+
+
+
+
