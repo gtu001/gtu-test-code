3594089afb2a00d62432753cf9568ee1d7b89a8a
gtu001 <gtu001@gmail.com>
2019-10-07 22:49:09 +0800

    ok_hone1

--- PythonGtu/gtu/os/runtimeUtil.py
+++ PythonGtu/gtu/os/runtimeUtil.py
@@ -1,6 +1,8 @@
 
 import subprocess 
 import os
+from gtu.reflect import checkSelf
+import io
 
 '''
 from gtu.os import runtimeUtil
@@ -26,3 +28,21 @@ def runtimeExec3(command, decode):
     out = str(out, decode)
     err = str(err, decode)
     return (out, err, errcode)
+
+
+def main() :
+    # checkSelf.checkMembersToHtml(io.TextIOWrapper, "io.TextIOWrapper")
+    command = "ls -al /media/gtu001/OLD_D"
+
+    process = subprocess.Popen(command, shell=True,
+                           stdout=subprocess.PIPE, 
+                           stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    errcode = process.returncode
+    checkSelf.checkMembersToHtml(process, "process")
+    print("errcodde", errcode)
+    print("done...")
+
+if __name__ == '__main__' : 
+    main()
+    
