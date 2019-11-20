7d36330a2fecb9fc17ffd685883efe9d717e131d
gtu001 <gtu001@gmail.com>
2019-11-20 13:39:21 +0800

    ok1120-1

--- GTU/src/gtu/jpg/FileToBmpUtilVer4_AES.java
+++ GTU/src/gtu/jpg/FileToBmpUtilVer4_AES.java
@@ -2,7 +2,14 @@ package gtu.jpg;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferShort;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.regex.Matcher;
@@ -14,18 +21,18 @@ import gtu.binary.EncryptAndDecryptHandler;
 import gtu.file.FileUtil;
 
 public class FileToBmpUtilVer4_AES {
-    
+
     private static EncryptAndDecryptHandler encryptAndDecryptHandler = new EncryptAndDecryptHandler("EVIL_GTU001_GOGO");
-    
+
     public static void main(String[] args) throws IOException {
         FileToBmpUtilVer4_AES t = FileToBmpUtilVer4_AES.getInstance();
 
-        int width = t.getWidth(new File(FileUtil.DESKTOP_DIR, "AlwaysOpenUI.exe"));
-        int fileLength = t.buildImageFromFile(new File(FileUtil.DESKTOP_DIR, "AlwaysOpenUI.exe"), new File(FileUtil.DESKTOP_DIR, "ttttt.bmp"), false, width);
+        int width = t.getWidth(new File(gtu.file.FileUtil.DESKTOP_DIR, "AlwaysOpenUI.exe"));
+        int fileLength = t.buildImageFromFile(new File(gtu.file.FileUtil.DESKTOP_DIR, "AlwaysOpenUI.exe"), new File(gtu.file.FileUtil.DESKTOP_DIR, "ttttt.bmp"), false, width);
         System.out.println(fileLength);
 
-        t.getFileFromImage(new File(FileUtil.DESKTOP_DIR, "ttttt.bmp"), //
-                new File(FileUtil.DESKTOP_DIR, "AlwaysOpenUI____2.exe"));
+        t.getFileFromImage(new File(gtu.file.FileUtil.DESKTOP_DIR, "ttttt.bmp"), //
+                new File(gtu.file.FileUtil.DESKTOP_DIR, "AlwaysOpenUI____2.exe"));
 
         System.out.println("done...");
     }
@@ -49,16 +56,16 @@ public class FileToBmpUtilVer4_AES {
     private static class ImageByteArryHeaderHandler {
         private static byte[] toImageBytes(byte[] bs) {
             System.out.println("[toImageBytes] orign size : " + bs.length);
-            
+
             bs = encryptAndDecryptHandler.encrypt(bs);
-            
+
             byte[] bsAndLengthMark = new byte[bs.length + 8];
             byte[] marks = ByteUtils.longToBytes(bs.length);
-            
+
             System.arraycopy(marks, 0, bsAndLengthMark, 0, 8);
             System.arraycopy(bs, 0, bsAndLengthMark, 8, bs.length);
             System.out.println("[toImageBytes] fix size : " + bsAndLengthMark.length);
-            
+
             return bsAndLengthMark;
         }
 
@@ -94,7 +101,7 @@ public class FileToBmpUtilVer4_AES {
                 mth.find();
                 toBmpFile = new File(toBmpFile.getParentFile(), mth.group(1) + "_" + orignLength + ".bmp");
             }
-            
+
             ImageIO.write(image, "bmp", toBmpFile);
             return bs.length;
         } catch (Exception ex) {
@@ -109,14 +116,61 @@ public class FileToBmpUtilVer4_AES {
         try {
             BufferedImage image = ImageIO.read(fromImageFile);
             byte[] imgData = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-            
-//            byte[] newArry = new byte[imgData.length - 1];
-//            System.arraycopy(imgData, 0, newArry, 0, newArry.length);
+
+            // byte[] newArry = new byte[imgData.length - 1];
+            // System.arraycopy(imgData, 0, newArry, 0, newArry.length);
             byte[] newArry = ImageByteArryHeaderHandler.getBytesFromImg(imgData);
 
             FileUtil.saveToFile(toFile, newArry);
         } catch (Exception ex) {
             throw new RuntimeException(ex);
+        }
+    }
+
+    private static class FileUtil {
+        public static void saveToFile(File file, byte[] data) {
+            try {
+                BufferedInputStream buffIn = new BufferedInputStream(new ByteArrayInputStream(data));
+                BufferedOutputStream buffOut = new BufferedOutputStream(new FileOutputStream(file));
+                byte[] arr = new byte[1024 * 1024];
+                int available = -1;
+                while ((available = buffIn.read(arr)) > 0) {
+                    buffOut.write(arr, 0, available);
+                }
+                buffOut.flush();
+                buffOut.close();
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static byte[] loadFileToByte(File file) throws IOException {
+            byte[] arrayOfByte = new byte[4096];
+            BufferedInputStream input = new BufferedInputStream(new FileInputStream(file));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int i;
+            while ((i = input.read(arrayOfByte, 0, arrayOfByte.length)) != -1) {
+                baos.write(arrayOfByte, 0, i);
+            }
+            baos.close();
+            input.close();
+            return baos.toByteArray();
+        }
+    }
+
+    private static class RawImageUtil {
+        public static BufferedImage short2Buffered(short[] pixels, int width, int height) throws IllegalArgumentException {
+            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_USHORT_GRAY);
+            short[] imgData = ((DataBufferShort) image.getRaster().getDataBuffer()).getData();
+            System.arraycopy(pixels, 0, imgData, 0, pixels.length);
+            return image;
+        }
+
+        public static BufferedImage byte2Buffered(byte[] pixels, int width, int height) throws IllegalArgumentException {
+            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+            byte[] imgData = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+            System.arraycopy(pixels, 0, imgData, 0, pixels.length);
+            return image;
         }
     }
 }
