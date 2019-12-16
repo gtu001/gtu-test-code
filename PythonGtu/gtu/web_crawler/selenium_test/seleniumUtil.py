2300184d1ae172ff10d68ac412067ba98036f6b5
gtu001 <gtu001@gmail.com>
2019-12-17 00:04:25 +0800

    ok1216-3

--- PythonGtu/gtu/web_crawler/selenium_test/seleniumUtil.py
+++ PythonGtu/gtu/web_crawler/selenium_test/seleniumUtil.py
@@ -10,6 +10,7 @@ from gtu.datetime import duringMointer
 from gtu.os import envUtil
 import inspect
 from gtu.string import stringUtil
+from selenium.webdriver.support.ui import WebDriverWait
 
 '''
 from gtu.web_crawler.selenium_test import seleniumUtil
@@ -98,7 +99,6 @@ class WebElementControl :
 
     @staticmethod
     def until(driver, timeout=300, frequence=0.5, message='', waitUntilFunc=None) :
-        from selenium.webdriver.support.wait import WebDriverWait
         #回傳False會繼續等
         if waitUntilFunc is None :
             def waitUntilFuncDefault(driver) :
@@ -109,6 +109,47 @@ class WebElementControl :
                     return True
             waitUntilFunc = waitUntilFuncDefault
         WebDriverWait(driver, timeout, frequence).until(waitUntilFunc, message)
+
+
+
+class DownloadWatcher :
+    @staticmethod
+    def getDownLoadedFileName_chrome(driver, waitTime):
+        driver.execute_script("window.open()")
+        driver.switch_to.window(driver.window_handles[-1])
+        driver.get('chrome://downloads')
+        endTime = time.time()+waitTime
+        while True:
+            try:
+                downloadPercentage = driver.execute_script(
+                    "return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('#progress').value")
+                if downloadPercentage == 100:
+                    return driver.execute_script("return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('div#content  #file-link').text")
+            except:
+                pass
+            time.sleep(1)
+            if time.time() > endTime:
+                break
+
+    @staticmethod
+    def getDownLoadedFileName_firefox(driver, waitTime):
+        driver.execute_script("window.open()")
+        WebDriverWait(driver,10).until(EC.new_window_is_opened)
+        driver.switch_to.window(driver.window_handles[-1])
+        driver.get("about:downloads")
+
+        endTime = time.time()+waitTime
+        while True:
+            try:
+                fileName = driver.execute_script("return document.querySelector('#contentAreaDownloadsView .downloadMainArea .downloadContainer description:nth-of-type(1)').value")
+                if fileName:
+                    return fileName
+            except:
+                pass
+            time.sleep(1)
+            if time.time() > endTime:
+                break
+
 
 
 class _PageChangeCallback :
