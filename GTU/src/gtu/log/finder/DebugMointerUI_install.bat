68e891e48f90d8dd013a0a8ec08d46edda9ccf0b
gtu001 <gtu001@gmail.com>
2020-04-08 09:42:14 +0800

    ok48-1

--- GTU/src/gtu/log/finder/DebugMointerUI_install.bat
+++ GTU/src/gtu/log/finder/DebugMointerUI_install.bat
@@ -1,11 +1,9 @@
-cd c:
+cd "%~dp0"
 
-cd C:\Users\gtu001\Desktop\my_tool
+"D:\JavaEnv4\apache-maven-3.6.0\bin\mvn.cmd" install:install-file -Dfile=DebugMointerUI.jar -DgroupId=gtu.log.finder -DartifactId=DebugMointerUI -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
 
-mvn install:install-file -Dfile=DebugMointerUI.jar -DgroupId=gtu.log.finder -DartifactId=DebugMointerUI -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+REM copy DebugMointerUI.jar c:\Oracle\Middleware\user_projects\domains\base_domain\lib\DebugMointerUI.jar /Y
 
-copy DebugMointerUI.jar c:\Oracle\Middleware\user_projects\domains\base_domain\lib\DebugMointerUI.jar /Y
-
-copy DebugMointerUI.jar C:\workspace\workspace_farEastStone\estore\fet_estore_search_engie_revamp\revamp_source_code\lib\include\DebugMointerUI.jar /Y
+REM copy DebugMointerUI.jar C:\workspace\workspace_farEastStone\estore\fet_estore_search_engie_revamp\revamp_source_code\lib\include\DebugMointerUI.jar /Y
 
 pause
