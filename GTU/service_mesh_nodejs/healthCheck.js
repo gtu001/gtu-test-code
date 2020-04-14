8557bc360a40ce0f53f397a8027de46f3e2442ee
gtu001 <gtu001@gmail.com>
2020-04-14 22:58:08 +0800

    ok414-2

---
+++ GTU/service_mesh_nodejs/healthCheck.js
@@ -0,0 +1,58 @@
+const express = require('express');
+
+// 建立 Router 物件
+const router = express.Router();
+const app = express();
+var port = process.env.PORT || 8000;
+
+// 舊方法
+app.get('/sample', function(req, res) {
+  res.send('this is a sample!');
+});
+
+// 在每一個請求被處理之前都會執行的 middleware
+router.use((req, res) => {
+  // res.json({ status: 'UP' });
+
+  // 輸出記錄訊息至終端機
+  console.log(req.method, req.url);
+
+  // 繼續路由處理
+  //next();
+});
+
+// Home page route.
+router.get('/', function (req, res) {
+  res.send('Wiki home page');
+});
+
+// About page route.
+router.get('/about', function (req, res) {
+  res.send('About this wiki');
+});
+
+
+// 驗證 :name 的 route middleware
+router.param('name', function(req, res, next, name) {
+  // 在這裡驗證資料
+  // ... ... ...
+
+  // 顯示驗證訊息
+  console.log('doing name validations on ' + name);
+
+  // 當驗證成功時，將其儲存至 req
+  req.name = name;
+
+  // 繼續後續的處理流程
+  //next();
+});
+
+
+// 將路由套用至應用程式
+app.use('/', router);
+// ---- 啟動伺服器 ----
+app.listen(port);
+
+module.exports = router;
+module.exports = app;
+
---
+++ GTU/service_mesh_nodejs/readme.md
@@ -0,0 +1,11 @@
+
+set PROJECT_ID=1
+
+docker build -t gcr.io/%PROJECT_ID%/service-mesh:v1 .
+
+docker run -p 8001:8001 gcr.io/%PROJECT_ID%/service-mesh:v1
+
+docker rmi gcr.io/1/service-mesh:v1
+
+
+//////https://medium.com/faun/building-your-very-own-service-mesh-4723895d061d
