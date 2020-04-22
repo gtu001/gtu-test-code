d6d6d2eefef9d01f3e639547fe9564b5dfedb934
gtu001 <gtu001@gmail.com>
2020-04-22 22:27:50 +0800

    ok422-f3

--- GTU/txt/derby/derby_run.sh
+++ GTU/txt/derby/derby_run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-export DERBY_HOME_PARAM="/media/gtu001/OLD_D/apps/db_tool/db-derby-10.12.1.1-bin"
+#/media/gtu001/OLD_D/apps/db_tool/db-derby-10.12.1.1-bin/bin/
 
-export DERBY_HOME="${DERBY_HOME_PARAM}"
+DERBY_HOME=$(dirname "$0")
+
+export DERBY_HOME_PARAM="${DERBY_HOME}"
+
+export DERBY_HOME="${DERBY_HOME}"
 
 java -jar ${DERBY_HOME}/lib/derbyrun.jar server start
--- GTU/txt/derby/derby_run1.sh
+++
@@ -1,6 +0,0 @@
-#/media/gtu001/OLD_D/apps/db_tool/db-derby-10.12.1.1-bin/bin/
-
-
-BASEDIR=$(dirname "$0")
-
-./${BASEDIR}/bin/startNetworkServer
