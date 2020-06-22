ce36feb08cbf8fd1f013b83451fc63c225855a52
gtu001 <gtu001@gmail.com>
2020-06-22 18:06:00 +0800

    ok622-4

--- GtuUnityMadBirds/Assets/scripts/Bird.cs
+++ GtuUnityMadBirds/Assets/scripts/Bird.cs
@@ -5,13 +5,19 @@ using static Log;
 using static Vector3Util;
 
 public class Bird : MonoBehaviour {
+
+	private void OnMouseDown() {
+		Log.debug("#Bird --" + "OnMouseDown");
+		GetComponent<SpriteRenderer>().color = Color.red;
+	}
+
     // Start is called before the first frame update
 	void Start () {
-
+		Log.debug("#Start --" + "start");
 	}
 
     // Update is called once per frame
 	void Update () {
-
+		Log.debug("#Update --" + "start");
 	}
 }
