470edafdfdd3d447705a81bba82d15cb2938b9a7
gtu001 <gtu001@gmail.com>
2020-07-03 17:57:42 +0800

    ok73-1

--- GtuUnityMadBirds/Assets/scripts/CameraBackgroundCtl.cs
+++ GtuUnityMadBirds/Assets/scripts/CameraBackgroundCtl.cs
@@ -18,27 +18,33 @@ public class CameraBackgroundCtl : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        Log.debug("Update --- start ");
+        //Log.debug("Update --- start ");
 
 
         Vector3 pos = transform.position;
-        Log.debug("Camera : " + pos);
+        //Log.debug("Camera : " + pos);
 
         var vertExtent = Camera.main.orthographicSize;    
         var horzExtent = vertExtent * Screen.width / Screen.height;
 
-        Log.debug("vertExtent " + vertExtent);
-        Log.debug("horzExtent " + horzExtent);
+        //Log.debug("vertExtent " + vertExtent);
+        //Log.debug("horzExtent " + horzExtent);
 
-        Log.debug("Rect " + mainCamera.rect);
+        //Log.debug("Rect " + mainCamera.rect);
 
         var backGround = GameObject.Find("Full-Background");
         // Log.showAll(backGround);
 
-        Log.debug("--->>> " + backGround.transform);
+        //Log.debug("--->>> " + backGround.transform);
 
         // RectTransform rt = backGround.transform.GetComponent<RectTransform>();
-        GameObjectUtil.showWidthHeight1(backGround);
+        //GameObjectUtil.showWidthHeight1(backGround);
+
+        
+        Vector2 screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
+        string posString = "[X : " + mainCamera.transform.position.x.ToString("N2") + ", Y : " + mainCamera.transform.position.y.ToString("N2") + "]";
+        this.name = "" + posString + ", Bound: " + screenBounds;
+        // Log.debug("screenBounds " + screenBounds);
     }
 
 
