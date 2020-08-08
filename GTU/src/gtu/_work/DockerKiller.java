b1b8843890b630e18475eb273d974c6460b09594
gtu001 <gtu001@gmail.com>
2020-08-09 00:05:06 +0800

    ok89-2

--- GTU/src/gtu/_work/DockerKiller.java
+++ GTU/src/gtu/_work/DockerKiller.java
@@ -1,10 +1,17 @@
 package gtu._work;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.reflect.FieldUtils;
+import org.apache.commons.lang3.ArrayUtils;
 
 import gtu.runtime.ProcessWatcher;
 import gtu.runtime.RuntimeBatPromptModeUtil;
@@ -31,11 +38,12 @@ public class DockerKiller {
         if (lst.size() <= 1) {
             return Collections.EMPTY_LIST;
         }
+        Map<String, Integer> firstBlocks = fistBlockSetting(lst.get(0));
         List<DockerImage> rtnLst = new ArrayList<DockerImage>();
         for (int ii = 1; ii < lst.size(); ii++) {
             String line = lst.get(ii);
-            List<String> blocks = divideToBlocks(line);
-            DockerImage image = blocksToImage(blocks);
+            Map<String, String> blocks = divideToBlocks(line, firstBlocks);
+            DockerImage image = blocksToBean(blocks, DockerImage.class);
             System.out.println(image);
             rtnLst.add(image);
         }
@@ -56,18 +64,74 @@ public class DockerKiller {
         if (lst.size() <= 1) {
             return Collections.EMPTY_LIST;
         }
+        Map<String, Integer> firstBlocks = fistBlockSetting(lst.get(0));
         List<DockerContainer> rtnLst = new ArrayList<DockerContainer>();
         for (int ii = 1; ii < lst.size(); ii++) {
             String line = lst.get(ii);
-            List<String> blocks = divideToBlocks(line);
-            DockerContainer container = blocksToContainer(blocks);
+            Map<String, String> blocks = divideToBlocks(line, firstBlocks);
+            DockerContainer container = blocksToBean(blocks, DockerContainer.class);
             System.out.println(container);
             rtnLst.add(container);
         }
         return rtnLst;
     }
 
-    private class DockerContainer {
+    public void deleteContainer(String containerId) {
+        RuntimeBatPromptModeUtil _inst = RuntimeBatPromptModeUtil.newInstance();
+        _inst.runInBatFile(false);
+        _inst.command("docker rm " + containerId);
+        ProcessWatcher proc = ProcessWatcher.newInstance(_inst.apply());
+        proc.getStreamSync();
+        String errorMsg = proc.getErrorStreamToString();
+        String inputMsg = proc.getInputStreamToString();
+        System.out.println("ERR : " + errorMsg);
+        System.out.println("OUTPUT : " + inputMsg);
+    }
+
+    public void deleteImage(String imageId) {
+        RuntimeBatPromptModeUtil _inst = RuntimeBatPromptModeUtil.newInstance();
+        _inst.runInBatFile(false);
+        _inst.command("docker rmi " + imageId);
+        ProcessWatcher proc = ProcessWatcher.newInstance(_inst.apply());
+        proc.getStreamSync();
+        String errorMsg = proc.getErrorStreamToString();
+        String inputMsg = proc.getInputStreamToString();
+        System.out.println("ERR : " + errorMsg);
+        System.out.println("OUTPUT : " + inputMsg);
+    }
+
+    private Map<String, Integer> fistBlockSetting(String line) {
+        Map<String, Integer> setting = new LinkedHashMap<String, Integer>();
+        Pattern ptn = Pattern.compile("\\w+");
+        Matcher mth = ptn.matcher(line);
+        int latestPos = -1;
+        int latestEndPos = -1;
+        String tmpKey = "";
+        for (; mth.find();) {
+            boolean ignoreLatestPos = false;
+            latestEndPos = mth.start();
+            if (StringUtils.isNotBlank(tmpKey)) {
+                if (ArrayUtils.contains(new String[] { "CONTAINER", "IMAGE" }, tmpKey) && //
+                        StringUtils.equals(mth.group(), "ID")) {
+                    tmpKey = tmpKey + "_ID";
+                    ignoreLatestPos = true;
+                } else {
+                    setting.put(tmpKey, Math.abs(latestPos - latestEndPos));
+                }
+            }
+            if (!ignoreLatestPos) {
+                latestPos = mth.start();
+                tmpKey = mth.group();
+            }
+        }
+        if (StringUtils.isNotBlank(tmpKey)) {
+            setting.put(tmpKey, line.substring(latestEndPos).length());
+        }
+        System.out.println("setting : " + setting);
+        return setting;
+    }
+
+    public static class DockerContainer {
         String CONTAINER_ID;
         String IMAGE;
         String COMMAND;
@@ -76,91 +140,167 @@ public class DockerKiller {
         String PORTS;
         String NAMES;
 
+        public Object[] toRows() {
+            return new Object[] { CONTAINER_ID, IMAGE, COMMAND, CREATED, STATUS, PORTS, NAMES, this };
+        }
+
         @Override
         public String toString() {
             return "DockerContainer [CONTAINER_ID=" + CONTAINER_ID + ", IMAGE=" + IMAGE + ", COMMAND=" + COMMAND + ", CREATED=" + CREATED + ", STATUS=" + STATUS + ", PORTS=" + PORTS + ", NAMES="
                     + NAMES + "]";
         }
+
+        public String getCONTAINER_ID() {
+            return CONTAINER_ID;
+        }
+
+        public void setCONTAINER_ID(String cONTAINER_ID) {
+            CONTAINER_ID = cONTAINER_ID;
+        }
+
+        public String getIMAGE() {
+            return IMAGE;
+        }
+
+        public void setIMAGE(String iMAGE) {
+            IMAGE = iMAGE;
+        }
+
+        public String getCOMMAND() {
+            return COMMAND;
+        }
+
+        public void setCOMMAND(String cOMMAND) {
+            COMMAND = cOMMAND;
+        }
+
+        public String getCREATED() {
+            return CREATED;
+        }
+
+        public void setCREATED(String cREATED) {
+            CREATED = cREATED;
+        }
+
+        public String getSTATUS() {
+            return STATUS;
+        }
+
+        public void setSTATUS(String sTATUS) {
+            STATUS = sTATUS;
+        }
+
+        public String getPORTS() {
+            return PORTS;
+        }
+
+        public void setPORTS(String pORTS) {
+            PORTS = pORTS;
+        }
+
+        public String getNAMES() {
+            return NAMES;
+        }
+
+        public void setNAMES(String nAMES) {
+            NAMES = nAMES;
+        }
     }
 
-    private class DockerImage {
+    public static class DockerImage {
         String REPOSITORY;
         String TAG;
         String IMAGE_ID;
         String CREATED;
         String SIZE;
 
+        public Object[] toRows() {
+            return new Object[] { REPOSITORY, TAG, IMAGE_ID, CREATED, SIZE, this };
+        }
+
         @Override
         public String toString() {
             return "DockerImage [REPOSITORY=" + REPOSITORY + ", TAG=" + TAG + ", IMAGE_ID=" + IMAGE_ID + ", CREATED=" + CREATED + ", SIZE=" + SIZE + "]";
         }
+
+        public String getREPOSITORY() {
+            return REPOSITORY;
+        }
+
+        public void setREPOSITORY(String rEPOSITORY) {
+            REPOSITORY = rEPOSITORY;
+        }
+
+        public String getTAG() {
+            return TAG;
+        }
+
+        public void setTAG(String tAG) {
+            TAG = tAG;
+        }
+
+        public String getIMAGE_ID() {
+            return IMAGE_ID;
+        }
+
+        public void setIMAGE_ID(String iMAGE_ID) {
+            IMAGE_ID = iMAGE_ID;
+        }
+
+        public String getCREATED() {
+            return CREATED;
+        }
+
+        public void setCREATED(String cREATED) {
+            CREATED = cREATED;
+        }
+
+        public String getSIZE() {
+            return SIZE;
+        }
+
+        public void setSIZE(String sIZE) {
+            SIZE = sIZE;
+        }
     }
 
-    private DockerImage blocksToImage(List<String> lst) {
-        DockerImage t = new DockerImage();
-        for (int ii = 0; ii < 7; ii++) {
-            switch (ii) {
-            case 0:
-                t.REPOSITORY = lst.get(ii);
-                break;
-            case 1:
-                t.TAG = lst.get(ii);
-                break;
-            case 2:
-                t.IMAGE_ID = lst.get(ii);
-                break;
-            case 3:
-                t.CREATED = lst.get(ii);
-                break;
-            case 4:
-                t.SIZE = lst.get(ii);
-                break;
-            }
-        }
-        return t;
+    private String fixBlockTitleName(String fname) {
+        return fname.replaceAll("\\s", "_");
     }
 
-    private DockerContainer blocksToContainer(List<String> lst) {
-        DockerContainer t = new DockerContainer();
-        for (int ii = 0; ii < 7; ii++) {
-            switch (ii) {
-            case 0:
-                t.CONTAINER_ID = lst.get(ii);
-                break;
-            case 1:
-                t.IMAGE = lst.get(ii);
-                break;
-            case 2:
-                t.COMMAND = lst.get(ii);
-                break;
-            case 3:
-                t.CREATED = lst.get(ii);
-                break;
-            case 4:
-                t.STATUS = lst.get(ii);
-                break;
-            case 5:
-                t.PORTS = lst.get(ii);
-                break;
-            case 6:
-                t.NAMES = lst.get(ii);
-                break;
+    private <T> T blocksToBean(Map<String, String> blocks, Class<T> clz) {
+        String fname = "";
+        try {
+            T obj = clz.newInstance();
+            for (String key : blocks.keySet()) {
+                fname = key;
+                String strVal = blocks.get(key);
+                fname = fixBlockTitleName(fname);
+                Field f = FieldUtils.getDeclaredField(clz, fname, true);
+                f.set(obj, strVal);
             }
+            return obj;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("[blocksToBean] ERR :" + e.getMessage() + " -- " + fname, e);
         }
-        return t;
     }
 
-    private List<String> divideToBlocks(String line) {
-        line = StringUtils.defaultString(line);
-        String[] arry = line.split("\\s{3}", -1);
-        List<String> lst = new ArrayList<String>();
-        for (String strVal : arry) {
-            strVal = StringUtils.trimToEmpty(strVal);
-            if (StringUtils.isBlank(strVal)) {
-                continue;
+    private Map<String, String> divideToBlocks(String line, Map<String, Integer> firstBlocks) {
+        Map<String, String> rtnMap = new LinkedHashMap<String, String>();
+        int startPos = 0;
+        int index = 0;
+        for (String key : firstBlocks.keySet()) {
+            int length = firstBlocks.get(key);
+            String strVal = line.substring(startPos, startPos + length);
+            if (index == firstBlocks.size() - 1) {
+                strVal = line.substring(startPos);
             }
-            lst.add(strVal);
+            System.out.println(key + "\t" + strVal);
+            startPos += length;
+            rtnMap.put(key, StringUtils.trimToEmpty(strVal));
+            index++;
         }
-        return lst;
+        return rtnMap;
     }
 }
---
+++ GTU/src/gtu/_work/ui/DockerKillerUI.java
@@ -0,0 +1,337 @@
+package gtu._work.ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.EventObject;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeListener;
+import javax.swing.table.DefaultTableModel;
+
+import gtu._work.DockerKiller;
+import gtu._work.DockerKiller.DockerContainer;
+import gtu._work.DockerKiller.DockerImage;
+import gtu._work.ui.JMenuBarUtil.JMenuAppender;
+import gtu.swing.util.HideInSystemTrayHelper;
+import gtu.swing.util.JCommonUtil;
+import gtu.swing.util.JFrameRGBColorPanel;
+import gtu.swing.util.JFrameUtil;
+import gtu.swing.util.JMouseEventUtil;
+import gtu.swing.util.JPopupMenuUtil;
+import gtu.swing.util.JTableUtil;
+import gtu.swing.util.SwingActionUtil;
+import gtu.swing.util.SwingActionUtil.Action;
+import gtu.swing.util.SwingActionUtil.ActionAdapter;
+
+public class DockerKillerUI extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    private HideInSystemTrayHelper hideInSystemTrayHelper;
+    private JFrameRGBColorPanel jFrameRGBColorPanel;
+    private SwingActionUtil swingUtil;
+    private JTabbedPane tabbedPane;
+    private JPanel panel_2;
+    private JPanel panel_3;
+    private JPanel panel_4;
+    private JPanel panel_5;
+    private JPanel panel_6;
+    private JPanel panel_7;
+    private JPanel panel_8;
+    private JPanel panel_9;
+    private JPanel panel_10;
+    private JTable psMinusATable;
+    private JTable imagesTable;
+    private JButton psMinusABtn;
+    private JButton imagesBtn;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        if (!JFrameUtil.lockInstance_delable(DockerKillerUI.class)) {
+            return;
+        }
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    DockerKillerUI frame = new DockerKillerUI();
+                    gtu.swing.util.JFrameUtil.setVisible(true, frame);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public DockerKillerUI() {
+        swingUtil = SwingActionUtil.newInstance(this);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 679, 469);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setContentPane(contentPane);
+
+        tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+        tabbedPane.addChangeListener((ChangeListener) ActionAdapter.ChangeListener.create(ActionDefine.JTabbedPane_ChangeIndex.name(), swingUtil));
+        contentPane.add(tabbedPane, BorderLayout.CENTER);
+
+        JPanel panel = new JPanel();
+        tabbedPane.addTab("PS-A", null, panel, null);
+        panel.setLayout(new BorderLayout(0, 0));
+
+        panel_3 = new JPanel();
+        panel.add(panel_3, BorderLayout.NORTH);
+
+        psMinusABtn = new JButton("重新整理");
+        psMinusABtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                swingUtil.invokeAction("psMinusABtn.click", arg0);
+            }
+        });
+        panel_3.add(psMinusABtn);
+
+        panel_4 = new JPanel();
+        panel.add(panel_4, BorderLayout.WEST);
+
+        panel_5 = new JPanel();
+        panel.add(panel_5, BorderLayout.EAST);
+
+        panel_6 = new JPanel();
+        panel.add(panel_6, BorderLayout.SOUTH);
+
+        psMinusATable = new JTable();
+        JTableUtil.defaultSetting(psMinusATable);
+        panel.add(JTableUtil.getScrollPane(psMinusATable), BorderLayout.CENTER);
+
+        psMinusATable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                swingUtil.invokeAction("psMinusATable.click", e);
+            }
+        });
+
+        JPanel panel_1 = new JPanel();
+        tabbedPane.addTab("IMAGES", null, panel_1, null);
+        panel_1.setLayout(new BorderLayout(0, 0));
+
+        panel_7 = new JPanel();
+        panel_1.add(panel_7, BorderLayout.NORTH);
+
+        imagesBtn = new JButton("重新整理");
+        imagesBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                swingUtil.invokeAction("imagesBtn.click", e);
+            }
+        });
+        panel_7.add(imagesBtn);
+
+        panel_8 = new JPanel();
+        panel_1.add(panel_8, BorderLayout.WEST);
+
+        panel_9 = new JPanel();
+        panel_1.add(panel_9, BorderLayout.EAST);
+
+        panel_10 = new JPanel();
+        panel_1.add(panel_10, BorderLayout.SOUTH);
+
+        imagesTable = new JTable();
+        JTableUtil.defaultSetting(imagesTable);
+        panel_1.add(JTableUtil.getScrollPane(imagesTable), BorderLayout.CENTER);
+
+        imagesTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                swingUtil.invokeAction("imagesTable.click", e);
+            }
+        });
+
+        panel_2 = new JPanel();
+        tabbedPane.addTab("其他設定", null, panel_2, null);
+        panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+        {
+            // 掛載所有event
+            applyAllEvents();
+
+            JCommonUtil.setJFrameCenter(this);
+            JCommonUtil.setJFrameIcon(this, "resource/images/ico/docker.ico");
+            hideInSystemTrayHelper = HideInSystemTrayHelper.newInstance();
+            hideInSystemTrayHelper.apply(this);
+            jFrameRGBColorPanel = new JFrameRGBColorPanel(this);
+            panel_2.add(jFrameRGBColorPanel.getToggleButton(false));
+            panel_2.add(hideInSystemTrayHelper.getToggleButton(false));
+            this.applyAppMenu();
+            JCommonUtil.defaultToolTipDelay();
+            this.setTitle("You Set My World On Fire");
+        }
+    }
+
+    private enum ActionDefine {
+        TEST_DEFAULT_EVENT, //
+        JTabbedPane_ChangeIndex, //
+        ;
+    }
+
+    private void applyAllEvents() {
+        swingUtil.addActionHex(ActionDefine.TEST_DEFAULT_EVENT.name(), new Action() {
+            @Override
+            public void action(EventObject evt) throws Exception {
+                System.out.println("====Test Default Event!!====");
+            }
+        });
+        swingUtil.addActionHex(ActionDefine.JTabbedPane_ChangeIndex.name(), new Action() {
+            @Override
+            public void action(EventObject evt) throws Exception {
+                System.out.println("tabbedPane : " + tabbedPane.getSelectedIndex());
+            }
+        });
+        swingUtil.addActionHex("psMinusABtn.click", new Action() {
+            @Override
+            public void action(EventObject evt) throws Exception {
+                initPsMinusATable();
+            }
+        });
+        swingUtil.addActionHex("imagesBtn.click", new Action() {
+            @Override
+            public void action(EventObject evt) throws Exception {
+                initImageTable();
+            }
+        });
+
+        swingUtil.addActionHex("psMinusATable.click", new Action() {
+            @Override
+            public void action(EventObject evt) throws Exception {
+                int beanColIndex = 7;
+
+                JTable table = (JTable) evt.getSource();
+                JTableUtil tabUtl = JTableUtil.newInstance(table);
+                DefaultTableModel model = (DefaultTableModel) tabUtl.getModel();
+                int rowId = tabUtl.getRealSelectedRow();
+                int colId = tabUtl.getRealColumnPos(beanColIndex, table);
+                System.out.println("select : " + rowId + " , " + colId);
+                DockerContainer bean = (DockerContainer) table.getValueAt(rowId, colId);
+                final String deleteId = bean.getCONTAINER_ID();
+
+                if (JMouseEventUtil.buttonRightClick(1, (MouseEvent) evt)) {
+                    JPopupMenuUtil.newInstance((JComponent) evt.getSource())//
+                            .addJMenuItem("刪除", new ActionListener() {
+                                @Override
+                                public void actionPerformed(ActionEvent arg0) {
+                                    boolean deleteConfirm = JCommonUtil._JOptionPane_showConfirmDialog_yesNoOption("是否刪除：" + deleteId, "刪除");
+                                    if (deleteConfirm) {
+                                        mDockerKiller.deleteContainer(deleteId);
+                                        initPsMinusATable();
+                                    }
+                                }
+                            })//
+                            .applyEvent(evt)//
+                            .show();
+                }
+            }
+        });
+        swingUtil.addActionHex("imagesTable.click", new Action() {
+            @Override
+            public void action(EventObject evt) throws Exception {
+                int beanColIndex = 5;
+
+                JTable table = (JTable) evt.getSource();
+                JTableUtil tabUtl = JTableUtil.newInstance(table);
+                DefaultTableModel model = (DefaultTableModel) tabUtl.getModel();
+                int rowId = tabUtl.getRealSelectedRow();
+                int colId = tabUtl.getRealColumnPos(beanColIndex, table);
+                System.out.println("select : " + rowId + " , " + colId);
+                DockerImage bean = (DockerImage) table.getValueAt(rowId, colId);
+                final String deleteId = bean.getIMAGE_ID();
+
+                if (JMouseEventUtil.buttonRightClick(1, (MouseEvent) evt)) {
+                    JPopupMenuUtil.newInstance((JComponent) evt.getSource())//
+                            .addJMenuItem("刪除", new ActionListener() {
+                                @Override
+                                public void actionPerformed(ActionEvent arg0) {
+                                    boolean deleteConfirm = JCommonUtil._JOptionPane_showConfirmDialog_yesNoOption("是否刪除：" + deleteId, "刪除");
+                                    if (deleteConfirm) {
+                                        mDockerKiller.deleteImage(deleteId);
+                                        initImageTable();
+                                    }
+                                }
+                            })//
+                            .applyEvent(evt)//
+                            .show();
+                }
+            }
+        });
+    }
+
+    DockerKiller mDockerKiller = new DockerKiller();
+
+    private void initPsMinusATable() {
+        DefaultTableModel model = JTableUtil.createModel(true, "CONTAINER_ID", "IMAGE", "COMMAND", "CREATED", "STATUS", "PORTS", "NAMES", "bean");
+        List<DockerContainer> lst = mDockerKiller.ps_minus_a();
+        for (DockerContainer c : lst) {
+            model.addRow(c.toRows());
+        }
+        psMinusATable.setModel(model);
+
+        Map<String, Object> preferences = new HashMap<String, Object>();
+        Map<Integer, Integer> presetColumns = new HashMap<Integer, Integer>();
+        presetColumns.put(7, 0);
+        preferences.put("presetColumns", presetColumns);
+
+        JTableUtil.newInstance(psMinusATable).setColumnWidths_ByDataContent(psMinusATable, preferences, getInsets(), false);
+        System.out.println("psMinusATable size = " + model.getRowCount());
+    }
+
+    private void initImageTable() {
+        DefaultTableModel model = JTableUtil.createModel(true, "REPOSITORY", "TAG", "IMAGE_ID", "CREATED", "SIZE", "bean");
+        List<DockerImage> lst = mDockerKiller.images();
+        for (DockerImage c : lst) {
+            model.addRow(c.toRows());
+        }
+        imagesTable.setModel(model);
+
+        Map<String, Object> preferences = new HashMap<String, Object>();
+        Map<Integer, Integer> presetColumns = new HashMap<Integer, Integer>();
+        presetColumns.put(5, 0);
+        preferences.put("presetColumns", presetColumns);
+
+        JTableUtil.newInstance(psMinusATable).setColumnWidths_ByDataContent(imagesTable, preferences, getInsets(), false);
+        System.out.println("imagesTable size = " + model.getRowCount());
+    }
+
+    private void applyAppMenu() {
+        JMenu menu1 = JMenuAppender.newInstance("child_item")//
+                .addMenuItem("detail1", (ActionListener) ActionAdapter.ActionListener.create(ActionDefine.TEST_DEFAULT_EVENT.name(), getSwingUtil()))//
+                .getMenu();
+        JMenu mainMenu = JMenuAppender.newInstance("file")//
+                .addMenuItem("item1", null)//
+                .addMenuItem("item2", (ActionListener) ActionAdapter.ActionListener.create(ActionDefine.TEST_DEFAULT_EVENT.name(), getSwingUtil()))//
+                .addChildrenMenu(menu1)//
+                .getMenu();
+        JMenuBarUtil.newInstance().addMenu(mainMenu).apply(this);
+    }
+
+    public SwingActionUtil getSwingUtil() {
+        return swingUtil;
+    }
+}
