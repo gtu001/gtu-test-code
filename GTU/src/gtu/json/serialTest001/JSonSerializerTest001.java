596e9ceb93c39f8a1ec5a65f47bbc7b933b4b66a
gtu001 <gtu001@gmail.com>
2020-10-01 20:18:49 +0800

    ok101-f1

--- GTU/src/gtu/json/serialTest001/JSonSerializerTest001.java
+++ GTU/src/gtu/json/serialTest001/JSonSerializerTest001.java
@@ -8,23 +8,30 @@ import java.lang.annotation.Target;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
 import java.util.ListIterator;
 import java.util.Map;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.reflect.FieldUtils;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.BeanDescription;
+import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.fasterxml.jackson.databind.ser.BeanSerializerFactory;
 
 import lombok.Data;
@@ -62,8 +69,9 @@ public class JSonSerializerTest001 {
 
     @Data
     @JsonSerialize(using = CustomJsonSerializer.class) // 自己加的
+    @JsonDeserialize(using = CustomJsonDeserializer.class) // 自己加的
     public static class ZsDoCalLimitDatasResDTO {
-//        @JsonProperty("TTTTTT")
+        // @JsonProperty("TTTTTT")
         @MapAsField
         private Map<String, ZsDoCalLimitS122531322ResDTO> dynamicMap = null;
     }
@@ -92,6 +100,67 @@ public class JSonSerializerTest001 {
     @Target({ ElementType.FIELD })
     @Retention(RetentionPolicy.RUNTIME)
     public @interface MapAsField {
+    }
+
+    public static class CustomJsonDeserializer extends JsonDeserializer<Object> {
+
+        @Override
+        public Object deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            ObjectCodec oc = jsonParser.getCodec();
+            JsonNode node = oc.readTree(jsonParser);
+
+            Class createClz = null;
+
+            Class currentClz = jsonParser.getCurrentValue().getClass();
+            A: for (Field field : currentClz.getDeclaredFields()) {
+                if (field.getType().getAnnotation(JsonDeserialize.class) != null) {
+
+                    System.out.println(field.getName() + " ----- " + field.getType().getAnnotation(JsonDeserialize.class));
+                    JsonDeserialize anno = field.getType().getAnnotation(JsonDeserialize.class);
+                    if (CustomJsonDeserializer.class == anno.using()) {
+                        createClz = field.getType();
+                        break A;
+                    }
+                }
+            }
+
+            if (createClz != null) {
+                try {
+                    ObjectMapper mapper = new ObjectMapper();
+
+                    Object mapObj = createClz.newInstance();
+
+                    Map<String, Object> map = new HashMap<String, Object>();
+                    Iterator<String> it = node.fieldNames();
+                    while (it.hasNext()) {
+                        String name = it.next();
+                        JsonNode node1 = node.get(name);
+                        if (JsonNodeType.ARRAY == node1.getNodeType()) {
+                        } else if (JsonNodeType.BINARY == node1.getNodeType()) {
+                        } else if (JsonNodeType.BOOLEAN == node1.getNodeType()) {
+                        } else if (JsonNodeType.MISSING == node1.getNodeType()) {
+                        } else if (JsonNodeType.NULL == node1.getNodeType()) {
+                        } else if (JsonNodeType.NUMBER == node1.getNodeType()) {
+                        } else if (JsonNodeType.OBJECT == node1.getNodeType()) {
+
+                            ZsDoCalLimitS122531322ResDTO realObj = mapper.convertValue(node1, ZsDoCalLimitS122531322ResDTO.class);
+                            map.put(name, realObj);
+                        } else if (JsonNodeType.POJO == node1.getNodeType()) {
+                        } else if (JsonNodeType.STRING == node1.getNodeType()) {
+                            map.put(name, node1.asText());
+                        }
+                    }
+
+                    Field mapField = FieldUtils.getDeclaredField(createClz, "dynamicMap", true);
+                    mapField.set(mapObj, map);
+
+                    return mapObj;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
     }
 
     public static class CustomJsonSerializer extends JsonSerializer<Object> {
@@ -143,7 +212,7 @@ public class JSonSerializerTest001 {
         }
     }
 
-    public static void main(String[] args) throws JsonProcessingException {
+    public static void main(String[] args) throws IOException {
         String insuredId = "A123456789";
         ObjectMapper objectMapper = new ObjectMapper();
 
@@ -170,7 +239,12 @@ public class JSonSerializerTest001 {
         ZsDoCalLimitDetailResDTO.setDatas(ZsDoCalLimitDatasResDTO);
         zsDoCalLimitResDTO.setDetail(ZsDoCalLimitDetailResDTO);
 
-        String result = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(zsDoCalLimitResDTO);
-        System.out.println(result);
+        String resultStr = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(zsDoCalLimitResDTO);
+        System.out.println(resultStr);
+
+        // ======================================================================
+        ZsDoCalLimitResDTO revertObj = objectMapper.readValue(resultStr, ZsDoCalLimitResDTO.class);
+
+        System.out.println(ReflectionToStringBuilder.toString(revertObj, org.apache.commons.lang.builder.ToStringStyle.MULTI_LINE_STYLE));
     }
 }
