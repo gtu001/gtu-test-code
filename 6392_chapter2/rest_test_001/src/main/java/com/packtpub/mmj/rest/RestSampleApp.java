90fa4499c7272da4fe77222a63f2f72fc60a571b
gtu001 <gtu001@gmail.com>
2020-11-04 17:36:31 +0800

    ok114-1

--- 6392_chapter2/rest_test_001/src/main/java/com/packtpub/mmj/rest/RestSampleApp.java
+++ 6392_chapter2/rest_test_001/src/main/java/com/packtpub/mmj/rest/RestSampleApp.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestSampleApp {
     public static void main(String[] args) {
         SpringApplication.run(RestSampleApp.class, args);
+        //https://cloud.spring.io/spring-cloud-netflix/multi/multi__service_discovery_eureka_clients.html
     }
 }
