1bac8185801ae1eb250a68addfcef0b550596f43
gtu001 <gtu001@gmail.com>
2020-12-27 20:09:06 +0800

    ok1227-n7

--- GTU/src/gtu/_work/ui/ImagesViewerUI.java
+++ GTU/src/gtu/_work/ui/ImagesViewerUI.java
@@ -221,7 +221,21 @@ public class ImagesViewerUI extends JFrame {
                                     mShowImageHandler.showImage();
                                 }
                             })//
-                            .addJMenuItem("依照檔名排序", new ActionListener() {
+                            .addJMenuItem("符合視窗寬度", new ActionListener() {
+                                @Override
+                                public void actionPerformed(ActionEvent e) {
+                                    mShowImageHandler.setMatchHeight(false);
+                                    mShowImageHandler.showImage();
+                                }
+                            })//
+                            .addJMenuItem("符合視窗填滿", new ActionListener() {
+                                @Override
+                                public void actionPerformed(ActionEvent e) {
+                                    mShowImageHandler.setMatchHeight(null);
+                                    mShowImageHandler.showImage();
+                                }
+                            })//
+                            .addJMenuItem("依照檔名排序[重新整理]", new ActionListener() {
                                 @Override
                                 public void actionPerformed(ActionEvent e) {
                                     mShowImageHandler.setCurrentComparator(COMPARE_BY_NAME);
@@ -229,12 +243,18 @@ public class ImagesViewerUI extends JFrame {
                                     mShowImageHandler.showImage();
                                 }
                             })//
-                            .addJMenuItem("依照修改時間排序", new ActionListener() {
+                            .addJMenuItem("依照修改時間排序[重新整理]", new ActionListener() {
                                 @Override
                                 public void actionPerformed(ActionEvent e) {
                                     mShowImageHandler.setCurrentComparator(COMPARE_BY_CREATETIME);
                                     mShowImageHandler.apply(null, null);
                                     mShowImageHandler.showImage();
+                                }
+                            })//
+                            .addJMenuItem("開啟目錄", new ActionListener() {
+                                @Override
+                                public void actionPerformed(ActionEvent e) {
+                                    mShowImageHandler.showDirectory();
                                 }
                             })//
                             .show();
@@ -275,18 +295,28 @@ public class ImagesViewerUI extends JFrame {
         private File currentDir;
         private List<File> currentFilesLst;
         int currentIndex = 0;
-        boolean isMatchHeight = true;
+        Boolean isMatchHeight = true;
         Comparator<File> currentComparator;
 
         ShowImageHandler() {
         }
 
-        public void setMatchHeight(boolean isMatchHeight) {
+        public void setMatchHeight(Boolean isMatchHeight) {
             this.isMatchHeight = isMatchHeight;
         }
 
         public void setCurrentComparator(Comparator<File> currentComparator) {
             this.currentComparator = currentComparator;
+        }
+
+        private List<File> resetCurrentFileLst(List<File> currentFileLst1) {
+            List<File> currentFileLst2 = new ArrayList<File>();
+            for (File f : currentFileLst1) {
+                if (f != null && f.exists()) {
+                    currentFileLst2.add(f);
+                }
+            }
+            return currentFileLst2;
         }
 
         public void apply(File file, List<File> currentFileLst1) {
@@ -295,10 +325,20 @@ public class ImagesViewerUI extends JFrame {
                     currentComparator = COMPARE_BY_NAME;
                 }
                 if (file == null && (currentFileLst1 == null || currentFileLst1.isEmpty())) {
+                    boolean findOk = false;
                     if ((this.currentFilesLst != null && !this.currentFilesLst.isEmpty())) {
-                        currentFileLst1 = this.currentFilesLst;
-                    } else {
-                        JCommonUtil._jOptionPane_showMessageDialog_error("檔案不存在");
+                        List<File> lst = resetCurrentFileLst(this.currentFilesLst);
+                        if (!lst.isEmpty()) {
+                            currentFileLst1 = lst;
+                            findOk = true;
+                        }
+                    }
+                    if (!findOk && this.currentDir != null && this.currentDir.exists()) {
+                        file = this.currentDir;
+                        findOk = true;
+                    }
+                    if (!findOk) {
+                        JCommonUtil._jOptionPane_showMessageDialog_error("檔案不存在[789]");
                         return;
                     }
                 }
@@ -326,6 +366,14 @@ public class ImagesViewerUI extends JFrame {
                         this.file = currentFilesLst.get(0);
                         this.currentIndex = 0;
                     }
+                }
+                if (this.file == null) {
+                    JCommonUtil._jOptionPane_showMessageDialog_error("目前無任何圖片[123]");
+                    return;
+                }
+                if (currentFilesLst == null || currentFilesLst.isEmpty()) {
+                    JCommonUtil._jOptionPane_showMessageDialog_error("目前無任何圖片[456]");
+                    return;
                 }
                 this.image = ImageUtil.getInstance().getBufferedImage(this.file);
                 String message = "[" + (currentIndex + 1) + "/" + currentFilesLst.size() + "]";
@@ -355,7 +403,9 @@ public class ImagesViewerUI extends JFrame {
                     }
                 }
             }
-            Collections.sort(currentFilesLst, currentComparator);
+            if (currentFilesLst != null && !currentFilesLst.isEmpty()) {
+                Collections.sort(currentFilesLst, currentComparator);
+            }
             if (this.file != null && this.file.exists()) {
                 for (int ii = 0; ii < currentFilesLst.size(); ii++) {
                     if (this.file.equals(currentFilesLst.get(ii))) {
@@ -414,12 +464,15 @@ public class ImagesViewerUI extends JFrame {
             }
             try {
                 int matchLength = ImageViewLbl.getHeight();
-                if (!isMatchHeight) {
+                if (isMatchHeight != null && !isMatchHeight) {
                     matchLength = ImageViewLbl.getWidth();
                 }
-                Image image2 = ImageUtil.getInstance().resizeImageByIndicateWH(image, isMatchHeight, matchLength);
-                // Image image2 = ImageUtil.getInstance().resizeImage(image,
-                // ImageViewLbl.getWidth(), ImageViewLbl.getHeight());
+                Image image2 = null;
+                if (isMatchHeight != null) {
+                    image2 = ImageUtil.getInstance().resizeImageByIndicateWH(image, isMatchHeight, matchLength);
+                } else {
+                    image2 = ImageUtil.getInstance().resizeImage(image, ImageViewLbl.getWidth(), ImageViewLbl.getHeight());
+                }
                 Icon image3 = ImageUtil.getInstance().imageToIcon(image2);
                 ImageViewLbl.setIcon(image3);
             } catch (Exception e) {
@@ -451,6 +504,7 @@ public class ImagesViewerUI extends JFrame {
                     if (this.file != null) {
                         currentFilesLst.remove(this.file);
                     }
+                    System.out.println("[deleteImage] currentFilesLst = " + currentFilesLst);
                     this.apply(null, currentFilesLst);
                     this.showImage();
                 }
@@ -459,6 +513,12 @@ public class ImagesViewerUI extends JFrame {
 
         private void showFile() {
             DesktopUtil.browseFileDirectory(file);
+        }
+
+        private void showDirectory() {
+            if (this.currentDir != null && this.currentDir.exists()) {
+                DesktopUtil.openDir(this.currentDir);
+            }
         }
     }
 
