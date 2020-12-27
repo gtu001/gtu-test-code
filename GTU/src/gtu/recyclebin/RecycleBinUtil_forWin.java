d8d33a467a872e61144023ba033581a9824db720
gtu001 <gtu001@gmail.com>
2020-12-27 18:05:26 +0800

    ok1227-n5

--- GTU/src/gtu/recyclebin/RecycleBinUtil_forWin.java
+++ GTU/src/gtu/recyclebin/RecycleBinUtil_forWin.java
@@ -13,7 +13,7 @@ public class RecycleBinUtil_forWin {
     }
 
     public static boolean moveTo(File fileOrDir) {
-        for (int time = 0; time < 10; time++) {
+        for (int time = 0; time < 20; time++) {
             try {
                 if (!fileOrDir.exists()) {
                     System.out.println("檔案不存在 : " + fileOrDir);
@@ -27,15 +27,7 @@ public class RecycleBinUtil_forWin {
                 System.out.println(resultStr);
                 return resultCode == 0;
             } catch (Exception e) {
-                if (e.getMessage().contains("程序無法存取檔案，因為檔案正由另一個程序使用。")) {
-                    try {
-                        Thread.currentThread().sleep(500);
-                    } catch (InterruptedException e1) {
-                    }
-                }
-                if (time > 10) {
-                    throw new RuntimeException("moveToRecycleBin ERR : " + e.getMessage(), e);
-                }
+                Thread.currentThread().sleep(500);
             }
         }
         return false;
