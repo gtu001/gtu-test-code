a436ab5ed2b187dd1583263752a107ba2f11c1cd
gtu001@gmail.com <gtu001@gmail.com>
2021-02-16 16:54:36 +0800

    ok春節216-f5

---
+++ GtuUnity/GtuFPS001/Assets/Scripts/CMouseLook2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMouseLook2 : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Log.debug("-----------------------------------start");
+
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Log.debug("xRotation : " + xRotation);
+        // Log.debug("mouseX : " + mouseX);
+        
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
